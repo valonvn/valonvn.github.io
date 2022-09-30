c3149e8d82da1a1f0b15d7379a359f3b1876cf15
valonvn <107135415+VSuiteOfficial@users.noreply.github.com>
2022-09-30 16:27:20 +0100

    revamp

--- script.js
+++
@@ -1,9 +0,0 @@
-var siteWidth = 1280;
-var scale = screen.width /siteWidth;
-
-document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
-
-$(".test").click(function() {
-  window.location = $(this).find("a").attr("href"); 
-  return false;
-});
